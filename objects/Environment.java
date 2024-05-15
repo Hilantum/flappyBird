6844aa3f30469e67bf6fe26843c179a2b71315a4
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-15 09:18:15 -0500

    Update Environment.java

--- objects/Environment.java
+++ objects/Environment.java
@@ -12,47 +12,56 @@ public class Environment extends JPanel {
     private BufferedImage background;
     private BufferedImage platform;
     private BufferedImage image;
+    private Player player;
 
     private int[][] backgroundValues = new int[3][2];
     private int[][] platformValues = new int[3][2];
 
-    public String currentBackground = "none";
-    public boolean moving = true;
-    
+    public boolean currentBackground = false;
+    public boolean transition = false;
+    public int currentModifier = 0;
+    public boolean moving = false;
 
-    public Environment() {
-        try { image = ImageIO.read(stream);
-        } catch (Exception exception) { 
+    public Environment(Player player) {
+        try {
+            image = ImageIO.read(stream);
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
 
+        this.player = player;
+
         addNormalBackground();
         resetBackground();
-        
+
         addPlatform();
         resetPlatform();
     }
 
     public void resetBackground() {
-        backgroundValues[0][0] = 0; backgroundValues[0][1] = 0;
-        backgroundValues[1][0] = 432; backgroundValues[1][1] = 0;
-        backgroundValues[2][0] = 864; backgroundValues[2][1] = 0;
+        backgroundValues[0][0] = 0;
+        backgroundValues[0][1] = 0;
+        backgroundValues[1][0] = 432;
+        backgroundValues[1][1] = 0;
+        backgroundValues[2][0] = 864;
+        backgroundValues[2][1] = 0;
     }
 
     public void resetPlatform() {
-        platformValues[0][0] = 0; platformValues[0][1] = 603;
-        platformValues[1][0] = 504; platformValues[1][1] = 603;
-        platformValues[2][0] = 1008; platformValues[2][1] = 603;
+        platformValues[0][0] = 0;
+        platformValues[0][1] = 603;
+        platformValues[1][0] = 504;
+        platformValues[1][1] = 603;
+        platformValues[2][0] = 1008;
+        platformValues[2][1] = 603;
     }
 
     public void addNormalBackground() {
-        currentBackground = "normal";
         background = image.getSubimage(0, 0, 144, 256);
     }
 
     public void addNightBackground() {
-        currentBackground = "night";
-        //.getSubimage(145, 0, 155, 256);
+        background = image.getSubimage(146, 0, 144, 256);
     }
 
     public void addPlatform() {
@@ -76,6 +85,21 @@ public class Environment extends JPanel {
                 platformValues[1][0] -= 1;
                 platformValues[2][0] -= 1;
             }
+
+            if (currentModifier >= 3000) {
+                currentBackground = !currentBackground;
+                currentModifier = 0;
+            } else {
+                currentModifier += 1;
+            }
+
+            if (!currentBackground) {
+                addNormalBackground();
+                player.currentSwitched = false;
+            } else {
+                addNightBackground();
+                player.currentSwitched = true;
+            }
         }
     }
 
@@ -84,12 +108,12 @@ public class Environment extends JPanel {
             for (int i = 0; i < 3; i++) {
                 G2D.drawImage(background, backgroundValues[i][0], backgroundValues[i][1], 144 * 3, 256 * 3, null);
             }
-    
+
             for (int i = 0; i < 3; i++) {
                 G2D.drawImage(platform, platformValues[i][0], platformValues[i][1], 168 * 3, 55 * 3, null);
             }
         } catch (Exception exception) {
             System.out.println("ERROR | Failed to display ENV background");
         }
-    }    
+    }
 }
