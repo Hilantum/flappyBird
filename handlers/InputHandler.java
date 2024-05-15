663c3c19ba92166c6d595ee51f9092ec1b73adc4
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-15 09:21:43 -0500

    Update InputHandler.java

--- handlers/InputHandler.java
+++ handlers/InputHandler.java
@@ -7,68 +7,49 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class InputHandler implements KeyListener, MouseListener {
-	private Environment environment;
-	private GamePanel panel;
-	private P player;
-	
-	public InputHandler(GamePanel panel, P player, Environment environment) {
-		this.panel = panel;
-		this.player = player;
-		this.environment = environment;
-	}
-	
-	/* Keyboard Methods */
-	@Override public void keyTyped(KeyEvent event) {
-		
-	}
+    private Environment environment;
+    private GamePanel panel;
+    private Player player;
 
-	@Override public void keyPressed(KeyEvent event) {
-		/*switch(event.getKeyCode()) {
-			case(KeyEvent.VK_A): player.move("left");
-			break;
-			
-			case(KeyEvent.VK_D): player.move("right");
-			break;
-			
-			case(KeyEvent.VK_SPACE): player.jump();
-			break;
-		}*/
+    public InputHandler(GamePanel panel, Player player, Environment environment) {
+        this.panel = panel;
+        this.player = player;
+        this.environment = environment;
+    }
 
-		switch(event.getKeyCode()) {
-			case(KeyEvent.VK_SPACE): player.flap();
-			break;
-		}
-	}
+    /* Keyboard Methods */
+    @Override public void keyTyped(KeyEvent event) {
 
-	@Override public void keyReleased(KeyEvent event) {
-		/*switch(event.getKeyCode()) {
-			case(KeyEvent.VK_A): player.stop("left");
-			break;
-			
-			case(KeyEvent.VK_D): player.stop("right");
-			break;
-		}*/
-	}
-	
-	/* Mouse Methods */
-	@Override public void mouseClicked(MouseEvent event) {
-		if (!player.playerBegun) {
-			environment.moving = true;
-			player.playerBegun = true;
-		} else {
-			player.flap();
-		}
-	}
+    }
 
-	@Override public void mousePressed(MouseEvent event) {
-		
-	}
+    @Override public void keyPressed(KeyEvent event) {
+        switch (event.getKeyCode()) {
+        case (KeyEvent.VK_SPACE):
+            player.flap();
+            break;
+        }
+    }
 
-	@Override public void mouseReleased(MouseEvent event) {
-		
-	}
+    @Override public void keyReleased(KeyEvent event) {}
 
-	// Not In Use \\
-	@Override public void mouseEntered(MouseEvent event) {}
-	@Override public void mouseExited(MouseEvent event) {}
+    /* Mouse Methods */
+    @Override public void mouseClicked(MouseEvent event) {
+        if (!player.playerBegun) {
+
+        } else {
+            player.flap();
+        }
+    }
+
+    @Override public void mousePressed(MouseEvent event) {
+
+    }
+
+    @Override public void mouseReleased(MouseEvent event) {
+
+    }
+
+    // Not In Use \\
+    @Override public void mouseEntered(MouseEvent event) {}
+    @Override public void mouseExited(MouseEvent event) {}
 }
