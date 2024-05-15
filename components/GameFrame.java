dab6d2e6ace82bae0db538a4396ab90d97062e2e
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-15 09:20:01 -0500

    Update GameFrame.java

--- components/GameFrame.java
+++ components/GameFrame.java
@@ -2,15 +2,15 @@ package components;
 
 import javax.swing.*;
 
-public class GameFrame extends JFrame {	
-	public GameFrame(GamePanel panel) {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.add(panel);
-		this.pack();
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-		
-		panel.requestFocus();
-	}
+public class GameFrame extends JFrame {
+    public GameFrame(GamePanel panel) {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        panel.requestFocus();
+    }
 }
