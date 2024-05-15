78206551cd727c9d42034154adf70945754a7a23
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-15 08:22:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,54 +1,57 @@
-import components.*; 
+import components.*;
 
 public class Main implements Runnable {
-	private GamePanel panel;
-	private GameFrame frame;
-	
-	public Main() {
-		panel = new GamePanel();
-		frame = new GameFrame(panel);
+   private GamePanel panel;
+   private GameFrame frame;
 
-		Thread thread = new Thread(this);
-		thread.start();
-	}
-	
-	@Override public void run() {
-		double frameTime = 1000000000 / Config.MAX_FRAMES;
-		double tickTime = 1000000000 / Config.MAX_TICKS;
-		double frameDelta = 0, tickDelta = 0;
+   public Main() {
+      panel = new GamePanel();
+      frame = new GameFrame(panel);
 
-		long lastSecondNano = System.nanoTime();
-		long lastDeltaNano = System.nanoTime();
+      Thread thread = new Thread(this);
+      thread.start();
+   }
 
-		int numFrames = 0, numTicks = 0;
-				
-		while (true) {
-			long currentNano = System.nanoTime();
-			
-			frameDelta += (currentNano - lastDeltaNano) / frameTime;
-			tickDelta += (currentNano - lastDeltaNano) / tickTime;
-			lastDeltaNano = currentNano;
-			
-			if (tickDelta >= 1) {
-				panel.getEnvironment().update();
-				panel.getPlayer().update();
-				tickDelta -= 1; numTicks += 1;
-			}
-			
-			if (frameDelta >= 1) {
-				panel.repaint();
-				frameDelta -= 1; numFrames += 1;
-			}
-			
-			if (currentNano - lastSecondNano >= 1000000000) {
-				System.out.println("FPS: " + numFrames + " TICKS: " + numTicks);
-				lastSecondNano = currentNano;
-				numFrames = 0; numTicks = 0;
-			}
-		}
-	}
-	
-	public static void main(String[] args) { 
-		new Main(); 
-	}
+   @Override public void run() {
+      double frameTime = 1000000000 / Config.MAX_FRAMES;
+      double tickTime = 1000000000 / Config.MAX_TICKS;
+      double frameDelta = 0, tickDelta = 0;
+
+      long lastSecondNano = System.nanoTime();
+      long lastDeltaNano = System.nanoTime();
+
+      int numFrames = 0, numTicks = 0;
+
+      while (true) {
+         long currentNano = System.nanoTime();
+
+         frameDelta += (currentNano - lastDeltaNano) / frameTime;
+         tickDelta += (currentNano - lastDeltaNano) / tickTime;
+         lastDeltaNano = currentNano;
+
+         if (tickDelta >= 1) {
+            panel.getEnvironment().update();
+            panel.getPlayer().update();
+            tickDelta -= 1;
+            numTicks += 1;
+         }
+
+         if (frameDelta >= 1) {
+            panel.repaint();
+            frameDelta -= 1;
+            numFrames += 1;
+         }
+
+         if (currentNano - lastSecondNano >= 1000000000) {
+            System.out.println("FPS: " + numFrames + " TICKS: " + numTicks);
+            lastSecondNano = currentNano;
+            numFrames = 0;
+            numTicks = 0;
+         }
+      }
+   }
+
+   public static void main(String[] args) {
+      new Main();
+   }
 }
