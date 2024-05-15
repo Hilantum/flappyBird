bbe37d7041f47c58ea7079649d1552f6bfdb86b2
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-15 09:17:46 -0500

    Update and rename P.java to Player.java

--- objects/Player.java
+++ objects/Player.java
@@ -1,26 +1,26 @@
 package objects;
 
 import java.awt.image.*;
-
-import javax.swing.JPanel;
-
+import java.awt.geom.*;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
+import handlers.*;
 
-import handlers.AH;
-
-public class P extends JPanel {
+public class Player extends JPanel {
     private BufferedImage[] current;
-    private AH AH = new AH();
+    private AnimationHandler AH = new AnimationHandler();
 
+    public boolean currentSwitched = false;
     private boolean scriptUpdating = false;
-    private boolean playerFlapping = false;
     private boolean playerJumping = false;
-    private boolean playerDead = false;
+
+    public boolean playerDead = false;
     public boolean playerBegun = false;
 
+    private int currentModifier = 0;
     private int animationSprite = 0;
     private int animationTick = 0;
+
     private int fallTick = 0;
 
     private double rotationalRadian = 0.0;
@@ -28,7 +28,7 @@ public class P extends JPanel {
     private int oldx = 0, oldy = 0;
     private int x = 0, y = 0;
 
-    public P(int x, int y) {
+    public Player(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -36,50 +36,49 @@ public class P extends JPanel {
     public void flap() {
         if (!playerJumping) {
             playerJumping = true;
-            oldx = x; oldy = y;
+            oldx = x;
+            oldy = y;
         } else {
-            oldx = x; oldy = y;
+            oldx = x;
+            oldy = y;
         }
     }
 
     public void update() {
-        if (scriptUpdating) { return; } else { scriptUpdating = true; }
+        if (scriptUpdating) {
+            return;
+        } else {
+            scriptUpdating = true;
+        }
 
         // State regulator
         if (playerDead) {
-            playerFlapping = false;
-            current = AH.getStationarySprites();
-            return;
-        } else {
-            playerFlapping = true;
+            y += 4;
+        } else if (!playerBegun) {
             current = AH.getFlappingSprites();
-        }
-
-        /*if (!playerBegun) { 
-            scriptUpdating = false;
-            return; 
-        }*/
-
-        // Tick Implementation with 8 revolutions p/second (200 ticks p/minute)
-        if (animationTick < 25) {
-            animationTick += 1;
+            animationSprite = 1;
         } else {
-            animationTick = 0;
+            if (currentSwitched) {
+                current = AH.getOtherFlappingSprites();
+            } else {
+                current = AH.getFlappingSprites();
+            }
 
-            // Sprite Implementation
-            if (playerFlapping) {
+            // Tick Implementation with 8 revolutions p/second (rate at 200 ticks p/second)
+            if (animationTick < 25) {
+                animationTick += 1;
+            } else {
+                animationTick = 0;
+
+                // Sprite Implementation
                 if (animationSprite >= (current.length) - 1) {
                     animationSprite = 0;
                 } else {
                     animationSprite += 1;
                 }
             }
-        }
 
-        // Physics Logic
-        if (fallTick < 50) {
-            fallTick += 1;
-        } else {
+            // Physics Logic
             if (!playerJumping) {
                 y += 1;
 
@@ -88,16 +87,17 @@ public class P extends JPanel {
                 }
             } else {
                 if (y > (oldy - 140)) {
-                    y -= 4;
+                    y -= 5;
                     rotationalRadian += .03;
                 } else {
                     playerJumping = false;
                 }
             }
-        }   
 
-        if (this.y >= 730 || this.y <= 0) {
-            this.playerDead = true;
+            if (this.y >= 730 || this.y <= 0) {
+                this.playerDead = true;
+                fallTick = 0;
+            }
         }
 
         scriptUpdating = false;
