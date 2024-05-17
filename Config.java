9bcd2061939d3bc39ac8665dc7e7b09d3ab91113
Hilantum <105911394+Hilantum@users.noreply.github.com>
2024-05-17 08:40:32 -0500

    Delete Config.java

--- Config.java
+++
@@ -1,9 +0,0 @@
-public class Config {
-   public static final int MAX_FRAMES = 120; // Maximum allowed frames per second
-   public static final int MAX_TICKS = 200; // Maximum server ticks allowed per second
-   //public static final int MAX_CACHE = 100; Maximum amount of cacheable images
-
-   public static final int IDLE = 1;
-   public static final int MOVING = 2;
-   public static final int JUMPING = 3;
-}
